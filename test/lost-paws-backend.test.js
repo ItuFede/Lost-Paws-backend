bdd54d66a36e53318eb2867cac639fca5d569866
ItuFede <federicoituarte1@gmail.com>
2024-09-04 23:30:04 -0300

    feature: CDK init and DynamoDB service

---
+++ test/lost-paws-backend.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const LostPawsBackend = require('../lib/lost-paws-backend-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lost-paws-backend-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LostPawsBackend.LostPawsBackendStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
