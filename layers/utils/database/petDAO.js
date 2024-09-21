6c528c50d010f4603561fc8ed88a98a9606c9f8d
ItuFede <federicoituarte1@gmail.com>
2024-09-21 00:20:30 -0300

    feature: DynamoDB - User - Vets and endpoint getVets

--- layers/utils/database/petDAO.js
+++ layers/utils/database/petDAO.js
@@ -1,4 +1,4 @@
-const db = require("database")
+const db = require("database");
 
 // TODO: Voy a tener que agregar un interface de los users y pets y un historial de las mascotas que se perdieron y encontraron
 
@@ -7,76 +7,76 @@ function generateTimestamp() {
 }
 
 const updateMissingPetState = async (tableName, id, position) => {
-
-  const { Item } = await db.get(tableName, {"id": id})
+  const { Item } = await db.get(tableName, { id: id });
 
   if (!Item) {
-    throw new Error(`Item with id ${id} not found`)
+    throw new Error(`Item with id ${id} not found`);
   }
 
   if (Item.isLost == "FALSE") {
-    throw new Error(`Pet id ${id} is not lost`)
+    throw new Error(`Pet id ${id} is not lost`);
   }
 
   // missingReports mas actual
-  const missingReports = Item.missingReports || []
+  const missingReports = Item.missingReports || [];
   const mostRecentReport = missingReports.reduce((latest, report) => {
-    return report.missingDate > latest.missingDate ? report : latest
-  }, missingReports[0])
+    return report.missingDate > latest.missingDate ? report : latest;
+  }, missingReports[0]);
 
   if (!mostRecentReport) {
-    throw new Error(`No missing reports found for item with id ${id}`)
+    throw new Error(`No missing reports found for item with id ${id}`);
   }
 
   const newLocation = {
     Latitud: position.Latitud,
     Longitud: position.Longitud,
     Timestamp: generateTimestamp(),
-  }
-  mostRecentReport.locationsView.push(newLocation)
-  mostRecentReport.state = "FOUND"
+  };
+  mostRecentReport.locationsView.push(newLocation);
+  mostRecentReport.state = "FOUND";
   const updatedItem = {
     isLost: "FALSE",
     missingReports,
-  }
+  };
 
   try {
-    let dbResultClients = await db.update(tableName, {"id": id}, updatedItem)
-    console.log("dbResultClients:::", dbResultClients)
-    return dbResultClients
-
+    let dbResult = await db.update(tableName, { id: id }, updatedItem);
+    console.log("dbResultPets:::", dbResult);
+    return dbResult;
   } catch (error) {
-    console.error('Error retrieving pets:', error);
-    return { success: false, message: 'Error retrieving pets', error };
+    console.error("Error retrieving pets:", error);
+    return { success: false, message: "Error retrieving pets", error };
   }
-
-}
-
+};
 
 const scanPet = async (tableName, { lastEvaluatedKey, pagination }) => {
-  console.log("In scanPet")
+  console.log("In scanPet");
 
-  let petScanCommand = {}
+  let petScanCommand = {};
 
-  if (lastEvaluatedKey && Object.keys(lastEvaluatedKey).length > 0) petScanCommand.ExclusiveStartKey = lastEvaluatedKey
+  if (lastEvaluatedKey && Object.keys(lastEvaluatedKey).length > 0)
+    petScanCommand.ExclusiveStartKey = lastEvaluatedKey;
 
   try {
-    console.log("PetScan:::", petScanCommand)
-    let dbResultClients = await db.scan(tableName, petScanCommand, pagination)
-    console.log("dbResultClients:::", dbResultClients)
+    console.log("PetScan:::", petScanCommand);
+    let dbResult = await db.scan(tableName, petScanCommand, pagination);
+    console.log("dbResultPets:::", dbResult);
 
-    if (!dbResultClients.Items) {
-      return
+    if (!dbResult.Items) {
+      return;
     }
 
-    return { pets: dbResultClients.Items, LastEvaluatedKey: dbResultClients.LastEvaluatedKey }
+    return {
+      pets: dbResult.Items,
+      LastEvaluatedKey: dbResult.LastEvaluatedKey,
+    };
   } catch (error) {
-    console.error('Error retrieving pets:', error);
-    return { success: false, message: 'Error retrieving pets', error };
+    console.error("Error retrieving pets:", error);
+    return { success: false, message: "Error retrieving pets", error };
   }
-}
+};
 
 module.exports = {
   updateMissingPetState,
-  scanPet
+  scanPet,
 };
---
+++ layers/utils/database/vetDAO.js
@@ -0,0 +1,32 @@
+const db = require("database");
+
+const scanVet = async (tableName, { lastEvaluatedKey, pagination }) => {
+  console.log("In scanVet");
+
+  let vetScanCommand = {};
+
+  if (lastEvaluatedKey && Object.keys(lastEvaluatedKey).length > 0)
+    vetScanCommand.ExclusiveStartKey = lastEvaluatedKey;
+
+  try {
+    console.log("VetScan:::", vetScanCommand);
+    let dbResult = await db.scan(tableName, vetScanCommand, pagination);
+    console.log("dbResultVets:::", dbResult);
+
+    if (!dbResult.Items) {
+      return;
+    }
+
+    return {
+      pets: dbResult.Items,
+      LastEvaluatedKey: dbResult.LastEvaluatedKey,
+    };
+  } catch (error) {
+    console.error("Error retrieving vets:", error);
+    return { success: false, message: "Error retrieving vets", error };
+  }
+};
+
+module.exports = {
+  scanVet,
+};
